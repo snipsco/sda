ba15c9aedaf9678ba0e568237e8587c49ebaf6ab
Mathieu Poumeyrol <kali@zoy.org>
2017-03-30 09:36:53 +0200

    simple aggregation creation from command line

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -32,6 +32,7 @@ fn main() {
         (@arg identity: -i --identity +takes_value "Storage directory for identity, including keys (defaults to .sda)")
         (@subcommand ping =>)
         (@subcommand agent =>
+            (about: "identity management")
             (@subcommand show =>)
             (@subcommand create =>
                 (@arg force: -f --force "Overwrite any existing identity")
@@ -41,7 +42,21 @@ fn main() {
                 (@subcommand create =>)
             )
         )
-        (@subcommand clerk =>)
+        (@subcommand clerk => (about: "run a clerk in a loop"))
+        (@subcommand aggregations =>
+            (about: "aggregations command")
+            (visible_aliases: &["agg", "aggs"])
+            (about: "manage aggregations")
+            (@subcommand create =>
+                (@arg title: "aggregation title")
+                (@arg dimension: "number of coefficient in the vector to be summed")
+                (@arg modulus: "modulus all cryptographic operation will operate on")
+                (@arg key: "key to use for recipient encryption")
+                (@arg share_count: "number of shares (and clerks)")
+                (@arg mask: --mask possible_value[none full chacha] default_value[none] "mask scheme")
+                (@arg sharing: --sharing possible_value[add shamir] default_value[add] "sharing scheme")
+            )
+        )
     ).get_matches();
 
     let root = slog_term::streamer().stderr().use_utc_timestamp().build().fuse();
@@ -74,10 +89,8 @@ fn run(matches: &clap::ArgMatches) -> SdaCliResult<()> {
         SdaHttpClient::new(server_root, authstore)?
     };
 
-    let identity_path = PathBuf::from(matches
-        .value_of("identity")
-        .unwrap_or(".sda")
-    );
+    let identity_path = PathBuf::from(matches.value_of("identity")
+        .unwrap_or(".sda"));
 
     let identitystore = {
         debug!("Using identity at {:?}", &identity_path);
@@ -98,16 +111,15 @@ fn run(matches: &clap::ArgMatches) -> SdaCliResult<()> {
         ("ping", Some(_)) => {
             let pong = service.ping()?;
             match pong {
-                Pong {running} if running => {
+                Pong { running } if running => {
                     info!("Service appears to be running");
                     Ok(())
-                },
-                _ => Err("Service may not be running")?
+                }
+                _ => Err("Service may not be running")?,
             }
-        },
+        }
 
         ("agent", Some(matches)) => {
-
             match matches.subcommand() {
 
                 ("create", Some(matches)) => {
@@ -122,20 +134,20 @@ fn run(matches: &clap::ArgMatches) -> SdaCliResult<()> {
                     };
                     let client = SdaClient::new(agent, keystore, Arc::new(service));
                     Ok(client.upload_agent()?)
-                },
+                }
 
                 ("show", Some(_)) => {
                     match agent {
                         None => {
                             warn!("No local agent found");
                             Ok(())
-                        },
+                        }
                         Some(agent) => {
                             info!("Local agent is {:?}", agent);
                             Ok(())
                         }
                     }
-                },
+                }
 
                 ("keys", Some(matches)) => {
                     let agent = agent.ok_or("Agent missing")?;
@@ -145,18 +157,18 @@ fn run(matches: &clap::ArgMatches) -> SdaCliResult<()> {
                         ("create", Some(_)) => {
                             let key = client.new_encryption_key()?;
                             client.upload_encryption_key(&key)?;
+                            info!("Created and uploaded key: {}", &key.to_string());
                             Ok(())
-                        },
+                        }
 
-                        (cmd, _) => Err(format!("Unknown subcommand {}", cmd))?
+                        (cmd, _) => Err(format!("Unknown subcommand {}", cmd))?,
                     }
-                },
+                }
 
-                (cmd, _) => Err(format!("Unknown subcommand {}",  cmd))?
+                (cmd, _) => Err(format!("Unknown subcommand {}", cmd))?,
 
             }
-
-        },
+        }
 
         ("clerk", Some(_matches)) => {
             let agent = agent.ok_or("Agent is needed. Maybe run \"sda agent create\" ?")?;
@@ -165,11 +177,64 @@ fn run(matches: &clap::ArgMatches) -> SdaCliResult<()> {
             loop {
                 debug!("Polling for clerking job");
                 client.run_chores(-1)?;
-                ::std::thread::sleep(::std::time::Duration::from_secs(5*60));
+                ::std::thread::sleep(::std::time::Duration::from_secs(5 * 60));
             }
         }
 
-        (cmd, _) => Err(format!("Unknown command {}", cmd))?
+        ("aggregations", Some(matches)) => {
+            match matches.subcommand() {
+                ("create", Some(matches)) => {
+                    let agent = agent.ok_or("Agent is needed. Maybe run \"sda agent create\" ?")?;
+                    service.ping()?;
+                    let modulus = value_t!(matches.value_of("modulus"), i64)
+                        .unwrap_or_else(|e| e.exit());
+                    let share_count = value_t!(matches.value_of("share_count"), usize)
+                        .unwrap_or_else(|e| e.exit());
+                    let sharing = match matches.value_of("sharing").unwrap() {
+                        "add" => {
+                            LinearSecretSharingScheme::Additive {
+                                modulus: modulus,
+                                share_count: share_count,
+                            }
+                        }
+                        "shamir" => unimplemented!(),
+                        _ => panic!(),
+                    };
+                    let masking = match matches.value_of("mask").unwrap() {
+                        "none" => LinearMaskingScheme::None,
+                        "full" => LinearMaskingScheme::Full { modulus: modulus },
+                        "chacha" => {
+                            LinearMaskingScheme::ChaCha {
+                                modulus: modulus,
+                                dimension: share_count,
+                                seed_bitsize: 128,
+                            }
+                        }
+                        _ => panic!(),
+                    };
+                    let agg = Aggregation {
+                        id: AggregationId::random(),
+                        title: matches.value_of("title").unwrap().to_string(),
+                        vector_dimension: value_t!(matches.value_of("dimension"), usize)
+                            .unwrap_or_else(|e| e.exit()),
+                        modulus: modulus,
+                        recipient: agent.id,
+                        recipient_key: value_t!(matches.value_of("key"), EncryptionKeyId)
+                            .unwrap_or_else(|e| e.exit()),
+                        committee_sharing_scheme: sharing,
+                        masking_scheme: masking,
+                        recipient_encryption_scheme: AdditiveEncryptionScheme::Sodium,
+                        committee_encryption_scheme: AdditiveEncryptionScheme::Sodium,
+                    };
+                    let res = service.create_aggregation(&agent, &agg)?;
+                    info!("aggregation created. id: {}", agg.id().to_string());
+                    Ok(())
+                }
+                (cmd, _) => Err(format!("Unknown command {}", cmd))?,
+            }
+        }
+
+        (cmd, _) => Err(format!("Unknown command {}", cmd))?,
     }
 
 }
